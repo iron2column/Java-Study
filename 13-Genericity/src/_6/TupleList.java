8b24f826611cecc57afdbbb65ea140b4eab9e033
陈明阳 <943609504@qq.com>
2021-08-24 16:47:24 +0800

    13 model - Genericity - 15.6完

---
+++ 13-Genericity/src/_6/TupleList.java
@@ -0,0 +1,15 @@
+package _6;
+
+import _2.FourTuple;
+
+import java.util.ArrayList;
+
+/**
+ * @Author ChenMingYang
+ * @Date 2021-08-24 14:07
+ */
+
+public class TupleList<A, B, C, D> extends ArrayList<FourTuple<A, B, C, D>> {
+    public static void main(String[] args) {
+    }
+}
